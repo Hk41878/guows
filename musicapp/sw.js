3155ff95634e42da16afdb66f0d2c2f1701d8bdf
Hk41878 <155512541+Hk41878@users.noreply.github.com>
2025-07-31 02:43:29 +0200

    Update sw.js

--- musicapp/sw.js
+++ musicapp/sw.js
@@ -1,24 +1,19 @@
 const CACHE_NAME = 'offline-music-v1';
 const FILES_TO_CACHE = [
   './',
-  './index.html',
-  './style.css',
-  './script.js',
-  './songs/song1.mp3'
+  './index.html'
+  // Songs will be cached on demand
 ];
 
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(FILES_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => {
-      return response || fetch(event.request);
-    })
+    caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
