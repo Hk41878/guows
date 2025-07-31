7be052dbea69d77e70736bf59abd65668159cc84
Hk41878 <155512541+Hk41878@users.noreply.github.com>
2025-07-31 04:25:28 +0200

    Update sw.js

--- musicapp/sw.js
+++ musicapp/sw.js
@@ -2,25 +2,23 @@ const CACHE_NAME = 'offline-music-v1';
 const FILES_TO_CACHE = [
   './',
   './index.html',
-  './icons/download.png',
-  './icons/remove.png'
+  './app.js',
+  './style.css'
 ];
 
 self.addEventListener('install', event => {
-  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  self.clients.claim();
+  event.waitUntil(self.clients.claim());
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(resp => {
-      return resp || fetch(event.request);
-    })
+    caches.match(event.request).then(resp => resp || fetch(event.request))
   );
 });
